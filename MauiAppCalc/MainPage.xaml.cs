04c004842a5638f880ee89489dd8d5f654fa552c
Usuario <Usuario@CPD03-M05>
2024-03-18 14:59:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppCalc/MainPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿namespace MauiAppCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void zerar_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void maismenos_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void porcento_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void dividir_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num7_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num8_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num9_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void multiplicar_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num4_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num5_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num6_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void subtrair_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num1_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num2_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num3_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void somar_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void num0_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void ponto_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void igual_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+    }
+
+}
